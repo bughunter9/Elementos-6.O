9fc59404bd37ca0341fb04ffbad1a67025c7bfba
Divjyot Singh <singhdivjyot911@gmail.com>
2021-03-15 11:36:38 +0530

    Fixed the images

--- src/components/About.js
+++ src/components/About.js
@@ -1,49 +1,48 @@
-import React from 'react';
-import './About.css';
+import React from "react";
+import "./About.css";
 
-function About(){
-    return(
-        <div className="about">
-            <div className="upper-part">
-                <div className="headin">
-                    <h1 > ABOUT ELEMENTOS 6.O</h1>
-                </div>
-                <div className="row">
-                    <div className="col1">
-                        <img 
-                        className="img1"
-                        src="https://www.burgerking.in/static/media/home-of-the-whopper.33b5f485.jpg"
-                        />
-                    </div>
-                    <div className="col1">
-                            <h4>
-                                CONTENT
-                            </h4>
-                    </div>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col2">
-                    <img
-                        src="https://www.burgerking.in/static/media/bk-wall-banner-3.4ab48ec9.jpg"
-                        
-                    />
-                </div>
-                <div className="col2">
-                    <img
-                        src="https://www.burgerking.in/static/media/bk-wall-banner-2.e1210188.jpg"
-                        
-                    />
-                </div>
-                <div className="col2">
-                    <img
-                        src="https://www.burgerking.in/static/media/bk-wall-banner-1.a84a9b57.jpg"
-                        
-                    />
-                </div>
-            </div>
+function About() {
+  return (
+    <div className="about">
+      <div className="upper-part">
+        <div className="headin">
+          <h1> ABOUT ELEMENTOS 6.O</h1>
         </div>
-    );
+        <div className="row">
+          <div className="col1">
+            <img
+              className="img1"
+              src="https://www.burgerking.in/static/media/home-of-the-whopper.33b5f485.jpg"
+              alt=""
+            />
+          </div>
+          <div className="col1">
+            <h4>CONTENT</h4>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col2">
+          <img
+            src="https://www.burgerking.in/static/media/bk-wall-banner-3.4ab48ec9.jpg"
+            alt=""
+          />
+        </div>
+        <div className="col2">
+          <img
+            src="https://www.burgerking.in/static/media/bk-wall-banner-2.e1210188.jpg"
+            alt=""
+          />
+        </div>
+        <div className="col2">
+          <img
+            src="https://www.burgerking.in/static/media/bk-wall-banner-1.a84a9b57.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
